6e96052e90bfac7c9ba3fdf43bd84330fad6d33c
Danny Shen <shenfive@gmail.com>
2022-10-15 09:59:22 +0800

    init

--- A902HelloCollectionView/ViewController.swift
+++ A902HelloCollectionView/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var theImages:[UIImage?] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            
+        theImages = [UIImage(named: "image1"),
+                     UIImage(named: "image2"),
+                     UIImage(named: "image3"),
+                     UIImage(named: "image4"),
+                     UIImage(named: "image5"),
+                     UIImage(named: "image6")]
+        print(theImages)
+        print()
     }
 
 
