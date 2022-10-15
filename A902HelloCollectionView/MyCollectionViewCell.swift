c3bae26ca6f002a91622221eae6b9d3cb27c7680
Danny Shen <shenfive@gmail.com>
2022-10-15 10:11:49 +0800

    CollectionViewCell

---
+++ A902HelloCollectionView/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  A902HelloCollectionView
+//
+//  Created by 申潤五 on 2022/10/15.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var theImageView: UIImageView!
+    
+}
